7aceb2c84beeeb42797bbe7c10dbcc23712703a9
R4mo-dev <121640965+R4mo-dev@users.noreply.github.com>
2024-02-13 18:57:25 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroButton = document.querySelector('.hero-button');
+    heroButton.classList.add('animate');
+
+    // Özelleştirilmiş animasyonlar ve etkileşimler için daha fazla kod ekleyin
+});
